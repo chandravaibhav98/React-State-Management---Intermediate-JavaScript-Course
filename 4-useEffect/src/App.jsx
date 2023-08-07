9b8ed48434ed0db60ca19d8cb4f39925d59b3701
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-08-07 23:38:13 +0530

    useEffect
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- 4-useEffect/src/App.jsx
+++ 4-useEffect/src/App.jsx
@@ -1,28 +1,47 @@
-import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-    const [names, setNames] = useState([]);
+	const [names, setNames] = useState([]);
 
-    useEffect(() => {
-        fetch('/names.json')
-            .then((response) => response.json())
-            .then((data) => setNames(data))
-    }, []);
+	useEffect(() => {
+		fetch('/names.json')
+			.then((response) => response.json())
+			.then((data) => setNames(data));
+	}, []);
 
-    const [selectedName, setSelectedName] = useState(null);
+	const [selectedName, setSelectedName] = useState(null);
+	const [selectedNameDetails, setSelectedNameDetails] = useState(null);
 
-        
+	useEffect(() => {
+        if (selectedName) {
+            fetch(`/${selectedName}.json`)
+				.then((response) => response.json())
+				.then((data) => setSelectedNameDetails(data));
+        }
+	}, [selectedName]);
 
-  return (
-    <>
-        <div>
-            <h5>Names:{names.join(", ")}</h5>
-        </div>
-    </>
-  )
+	return (
+		<div>
+			<div>
+				useEffect
+				<h5>Names:{names.join(', ')}</h5>
+				<main>
+					{names.map((name) => (
+						<button
+							key={name}
+							onClick={() => setSelectedName(name)}>
+							{name}
+						</button>
+					))}
+					<h6>{selectedName}</h6>
+					<h6>{JSON.stringify(selectedNameDetails)}</h6>
+				</main>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
